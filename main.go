92677d6856ca0049498a4659e37afd5b5f4c633b
Khadga Bahadur Shrestha <khadgalovecoding2016@gmail.com>
2025-06-30 05:01:47 +0545

    added health check route

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/kodega2016/femapi/internal/app"
 )
 
@@ -10,5 +14,22 @@ func main() {
 		panic(err)
 	}
 
-	app.Logger.Panicln("we are running our application...")
+	app.Logger.Println("we are running our application...")
+	http.HandleFunc("/health", HealthCheck)
+
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
 }
