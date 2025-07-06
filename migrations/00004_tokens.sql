d37fea26afafcf136a4df0e55ffc90d1964407cd
Khadga Bahadur Shrestha <khadgalovecoding2016@gmail.com>
2025-07-06 07:37:55 +0545

    fix:schema migration for tokens

--- migrations/00004_tokens.sql
+++ migrations/00004_tokens.sql
@@ -1,13 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
-
 CREATE TABLE IF NOT EXISTS tokens (
     hash BYTEA PRIMARY KEY,
     user_id BIGINT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
     expiry TIMESTAMP(0) WITH TIME ZONE NOT NULL,
-    scopre TEXT NOT NULL
+    scope TEXT NOT NULL
 );
--- +gooose StatementEnd
+-- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
