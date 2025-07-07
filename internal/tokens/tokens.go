84f243219c76db4e2fc52addefa112e1f9f31016
Khadga Bahadur Shrestha <khadgalovecoding2016@gmail.com>
2025-07-07 06:51:12 +0545

    feat:added token generate function

--- internal/tokens/tokens.go
+++ internal/tokens/tokens.go
@@ -1,7 +1,16 @@
 // Package tokens is used to generate the auth token
 package tokens
 
-import "time"
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base32"
+	"time"
+)
+
+const (
+	ScopeAuth = "authentication"
+)
 
 type Token struct {
 	Plaintext string    `json:"token"`
@@ -9,4 +18,26 @@ type Token struct {
 	UserID    int       `json:"-"`
 	Expiry    time.Time `json:"expiry"`
 	Scope     string    `json:"-"`
+}
+
+func GenerateToken(UserID int, ttl time.Duration, scope string) (*Token, error) {
+	token := &Token{
+		UserID: UserID,
+		Expiry: time.Now().Add(ttl),
+	}
+
+	// make bytes address to hold the hash
+	emptyBytes := make([]byte, 32)
+
+	// fills the bytes with the secure random bytes
+	_, err := rand.Read(emptyBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(emptyBytes)
+	hash := sha256.Sum224([]byte(token.Plaintext))
+	// convert this into the flexible slice type
+	token.Hash = hash[:]
+	return token, nil
 }
